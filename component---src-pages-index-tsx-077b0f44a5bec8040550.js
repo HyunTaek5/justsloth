7af993a83bea41d727f463bba888a4b5d2878c2b
HyunTaek5 <oht222@naver.com>
2025-12-27 13:47:26 +0000

    Updates

---
+++ component---src-pages-index-tsx-077b0f44a5bec8040550.js
@@ -0,0 +1,14 @@
+"use strict";(self.webpackChunk_portfolio_blog=self.webpackChunk_portfolio_blog||[]).push([[348],{9836:function(e,t,i){i.r(t);var r=i(1004),a=i(6448),n=i(1228),s=i(9300),o=i(9296),l=i(6252),d=i(8440),g=i(7e3);const m=a.xZ`
+  0% {
+    transform: rotateZ(0);
+  }
+
+  50% {
+    transform: rotateZ(30deg);
+  }
+
+  100% {
+    transform: rotateZ(0deg);
+  }
+`,h=(0,r.c)("div",{target:"evz1g1x4"})({name:"8iutvn",styles:"margin-left:auto;margin-right:auto;max-width:900px;padding:2rem"}),c=(0,r.c)("header",{target:"evz1g1x3"})({name:"1j66vkv",styles:"padding:2rem 0"}),x=(0,r.c)("div",{target:"evz1g1x2"})("margin-bottom:1.5rem;max-width:550px;&>span:first-of-type{position:relative;&::after{position:absolute;right:-4rem;top:-0.5rem;content:'✋';display:block;animation:",m," 1s ease-in-out infinite;}}"),p=(0,r.c)("div",{target:"evz1g1x1"})({name:"1i7yo1v",styles:"max-width:550px;margin-bottom:2rem"}),u=(0,r.c)("section",{target:"evz1g1x0"})({name:"1jwuio6",styles:"width:100%;margin-left:auto;margin-right:auto;max-width:900px;padding:2rem"});t.default=e=>{const{data:t}=e,{nodes:i}=t.allMarkdownRemark;return(0,g.jsxs)(n.q4,{children:[(0,g.jsx)(l.y,{name:"홈"}),(0,g.jsx)(c,{children:(0,g.jsxs)(h,{children:[(0,g.jsxs)(x,{children:[(0,g.jsx)(n.a,{as:"span",size:48,weight:800,children:"어서오세요!"}),(0,g.jsx)(n.a,{as:"h1",size:48,weight:800,lineHeight:56,children:"저는 오현택입니다."})]}),(0,g.jsx)(p,{children:(0,g.jsxs)(n.a,{as:"p",size:20,weight:400,lineHeight:40,children:["대한민국 서울에서 ",(0,g.jsx)("strong",{children:"소프트웨어 엔지니어"}),"로 일하고 있어요. 한 분야에 국한되지 않고, 다양한 도메인을 접해보는 것에 즐거움을 느낍니다. 공부하면서, 또는 이슈 대응하면서 알게된 점 위주로 블로그에 글을 작성하고 있어요. 최근에는 Go 언어 생태계에 관심이 많아, 많은 시간을 투자하고 있어요."]})}),(0,g.jsx)(n.q,{size:"large",color:"alert",onClick:()=>{return e="/posts",void(0,s.navigate)(e);var e},children:(0,g.jsx)(n.a,{size:16,weight:800,children:"블로그 글 보러갈까요?"})})]})}),(0,g.jsx)(u,{children:(0,g.jsx)(o.e,{title:"최근 글",list:(r=i,(0,d.iw)(r).filter(((e,t)=>t<6)).map((e=>({name:e.frontmatter.title,slug:e.fields.slug,date:a(e.frontmatter.date)}))))})})]});var r;function a(e){const[,t,i]=(0,d.HI)(e);return`${Number(t)}월 ${Number(i)}일`}}}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-077b0f44a5bec8040550.js.map
